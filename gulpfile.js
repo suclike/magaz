43e1e289d44dd02fe1cb9a0cf4bf6d7334c09c2e
Ahmad Ajmi <ahmad@ahmadajmi.com>
2016-08-05 14:24:55 +0200

    Update readme & gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,65 @@
-'use strict';
+(function() {
 
-// Include Gulp & Tools
+  'use strict';
 
-var gulp         = require('gulp');
-var watch        = require('gulp-watch');
-var sass         = require('gulp-sass');
-var rtlcss       = require('gulp-rtlcss');
-var minifycss    = require('gulp-minify-css');
-var autoprefixer = require('gulp-autoprefixer');
-var runSequence  = require('run-sequence');
-var concat       = require('gulp-concat');
-var rename       = require('gulp-rename');
-var uglify       = require('gulp-uglify');
-var plumber      = require('gulp-plumber');
-var gutil        = require('gulp-util');
+  // Include Gulp & Tools
+  var gulp         = require('gulp');
+  var watch        = require('gulp-watch');
+  var sass         = require('gulp-sass');
+  var rtlcss       = require('gulp-rtlcss');
+  var minifycss    = require('gulp-minify-css');
+  var autoprefixer = require('gulp-autoprefixer');
+  var runSequence  = require('run-sequence');
+  var concat       = require('gulp-concat');
+  var rename       = require('gulp-rename');
+  var uglify       = require('gulp-uglify');
+  var plumber      = require('gulp-plumber');
+  var gutil        = require('gulp-util');
 
-var onError = function(err) {
-  console.log('An error occurred:', gutil.colors.magenta(err.message));
-  gutil.beep();
-  this.emit('end');
-}
+  var onError = function(err) {
+    console.log('An error occurred:', gutil.colors.magenta(err.message));
+    gutil.beep();
+    this.emit('end');
+  };
 
-// SASS
-
-gulp.task('sass', function () {
-  return gulp.src('./sass/*.scss')
-  .pipe(plumber({ errorHandler: onError }))
-  .pipe(sass({outputStyle: 'expanded'}))
-  .pipe(autoprefixer())
-  .pipe(gulp.dest('./'))              // Output LTR stylesheets
-  .pipe(rtlcss())                     // Convert to RTL
-  .pipe(rename({ basename: 'rtl' }))  // Append "-rtl" to the filename
-  .pipe(gulp.dest('./'));             // Output RTL stylesheets
-});
-
-// JavaScript
-
-gulp.task('js', function() {
-  return gulp.src([
-    './js/scripts/jquery-2.1.4.min.js',
-    './js/scripts/jquery.fitvids.js',
-    './js/scripts/skip-link-focus-fix.js',
-    './js/scripts/script.js'])
+  // SASS
+  gulp.task('sass', function () {
+    return gulp.src('./sass/*.scss')
     .pipe(plumber({ errorHandler: onError }))
-    .pipe(concat('app.js'))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(uglify())
-    .pipe(gulp.dest('./js'))
-});
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./'))              // Output LTR stylesheets
+    .pipe(rtlcss())                     // Convert to RTL
+    .pipe(rename({ basename: 'rtl' }))  // Append "-rtl" to the filename
+    .pipe(gulp.dest('./'));             // Output RTL stylesheets
+  });
 
-// Watch
+  // JavaScript
+  gulp.task('js', function() {
+    return gulp.src([
+      './js/scripts/jquery-2.1.4.min.js',
+      './js/scripts/jquery.fitvids.js',
+      './js/scripts/skip-link-focus-fix.js',
+      './js/scripts/script.js'])
+      .pipe(plumber({ errorHandler: onError }))
+      .pipe(concat('app.js'))
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(uglify())
+      .pipe(gulp.dest('./js'))
+  });
 
-gulp.task('watch', function() {
-  gulp.watch('./sass/**/*.scss', ['sass']);
-  gulp.watch('./js/scripts/**/*.js', ['js']);
-});
+  // Watch
+  gulp.task('watch', function() {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+    gulp.watch('./js/scripts/**/*.js', ['js']);
+  });
 
-// Build
+  // Build
+  gulp.task('build', [], function() {
+    runSequence('sass', 'js');
+  });
 
-gulp.task('build', [], function() {
-  runSequence('sass', 'js');
-});
+  // Default
+  gulp.task('default', ['build', 'watch']);
 
-// Default
-
-gulp.task('default', ['build', 'watch']);
+})();
