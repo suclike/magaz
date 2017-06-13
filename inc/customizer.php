4a2a60510cbfc769d077dcedc045ad11bbdd429c
Ahmad Ajmi <ahmad@ahmadajmi.com>
2017-06-13 18:53:36 +0200

    Updated tags removed custom header support

--- inc/customizer.php
+++ inc/customizer.php
@@ -10,7 +10,7 @@
  *
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
-function aspire_customize_register( $wp_customize ) {
+function magaz_customize_register( $wp_customize ) {
   // Remove colors, background_image sections from theme customizer
   $wp_customize->remove_section( 'colors' );
   $wp_customize->remove_section( 'background_image' );
@@ -20,4 +20,4 @@ function aspire_customize_register( $wp_customize ) {
   $wp_customize->remove_control( 'header_image' );
   $wp_customize->remove_control( 'display_header_text' );
 }
-add_action( 'customize_register', 'aspire_customize_register' );
+add_action( 'customize_register', 'magaz_customize_register' );
