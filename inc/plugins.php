8d9c63379d7bfa7680783b52e52b22e3355ba0ab
Ahmad Ajmi <ahmad@ahmadajmi.com>
2017-06-23 18:13:38 +0200

    Removed duplicated code from TGM-Plugin-Activation and added missing translation Make WPCustom Category Image plugin to be recommended instead of required

--- inc/plugins.php
+++ inc/plugins.php
@@ -37,7 +37,7 @@ function magaz_register_required_plugins() {
 		array(
 			'name'      => 'WPCustom Category Image',
 			'slug'      => 'wpcustom-category-image',
-			'required'  => true,
+			'required'  => false,
 		),
 
 	);
@@ -60,81 +60,6 @@ function magaz_register_required_plugins() {
 		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
 		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
 		'message'      => '',                      // Message to output right before the plugins table.
-		'strings'      => array(
-			'page_title'                      => esc_html__( 'Install Required Plugins', 'magaz' ),
-			'menu_title'                      => esc_html__( 'Install Plugins', 'magaz' ),
-			'installing'                      => esc_html__( 'Installing Plugin: %s', 'magaz' ), // %s = plugin name.
-			'oops'                            => esc_html__( 'Something went wrong with the plugin API.', 'magaz' ),
-			'notice_can_install_required'     => _n_noop(
-				'This theme requires the following plugin: %1$s.',
-				'This theme requires the following plugins: %1$s.',
-				'magaz'
-			), // %1$s = plugin name(s).
-			'notice_can_install_recommended'  => _n_noop(
-				'This theme recommends the following plugin: %1$s.',
-				'This theme recommends the following plugins: %1$s.',
-				'magaz'
-			), // %1$s = plugin name(s).
-			'notice_cannot_install'           => _n_noop(
-				'Sorry, but you do not have the correct permissions to install the %1$s plugin.',
-				'Sorry, but you do not have the correct permissions to install the %1$s plugins.',
-				'magaz'
-			), // %1$s = plugin name(s).
-			'notice_ask_to_update'            => _n_noop(
-				'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.',
-				'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.',
-				'magaz'
-			), // %1$s = plugin name(s).
-			'notice_ask_to_update_maybe'      => _n_noop(
-				'There is an update available for: %1$s.',
-				'There are updates available for the following plugins: %1$s.',
-				'magaz'
-			), // %1$s = plugin name(s).
-			'notice_cannot_update'            => _n_noop(
-				'Sorry, but you do not have the correct permissions to update the %1$s plugin.',
-				'Sorry, but you do not have the correct permissions to update the %1$s plugins.',
-				'magaz'
-			), // %1$s = plugin name(s).
-			'notice_can_activate_required'    => _n_noop(
-				'The following required plugin is currently inactive: %1$s.',
-				'The following required plugins are currently inactive: %1$s.',
-				'magaz'
-			), // %1$s = plugin name(s).
-			'notice_can_activate_recommended' => _n_noop(
-				'The following recommended plugin is currently inactive: %1$s.',
-				'The following recommended plugins are currently inactive: %1$s.',
-				'magaz'
-			), // %1$s = plugin name(s).
-			'notice_cannot_activate'          => _n_noop(
-				'Sorry, but you do not have the correct permissions to activate the %1$s plugin.',
-				'Sorry, but you do not have the correct permissions to activate the %1$s plugins.',
-				'magaz'
-			), // %1$s = plugin name(s).
-			'install_link'                    => _n_noop(
-				'Begin installing plugin',
-				'Begin installing plugins',
-				'magaz'
-			),
-			'update_link' 					  => _n_noop(
-				'Begin updating plugin',
-				'Begin updating plugins',
-				'magaz'
-			),
-			'activate_link'                   => _n_noop(
-				'Begin activating plugin',
-				'Begin activating plugins',
-				'magaz'
-			),
-			'return'                          => esc_html__( 'Return to Required Plugins Installer', 'magaz' ),
-			'plugin_activated'                => esc_html__( 'Plugin activated successfully.', 'magaz' ),
-			'activated_successfully'          => esc_html__( 'The following plugin was activated successfully:', 'magaz' ),
-			'plugin_already_active'           => esc_html__( 'No action taken. Plugin %1$s was already active.', 'magaz' ),  // %1$s = plugin name(s).
-			'plugin_needs_higher_version'     => esc_html__( 'Plugin not activated. A higher version of %s is needed for this theme. Please update the plugin.', 'magaz' ),  // %1$s = plugin name(s).
-			'complete'                        => esc_html__( 'All plugins installed and activated successfully. %1$s', 'magaz' ), // %s = dashboard link.
-			'contact_admin'                   => esc_html__( 'Please contact the administrator of this site for help.', 'magaz' ),
-
-			'nag_type'                        => 'updated', // Determines admin notice type - can only be 'updated', 'update-nag' or 'error'.
-		),
 	);
 
 	tgmpa( $plugins, $config );
